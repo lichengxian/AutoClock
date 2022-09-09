366f8fb4b6d750c2996c5e79a42ef51fdacb47c7
lichengxian <59009875+lichengxian@users.noreply.github.com>
2022-09-09 10:39:36 +0800

    完成自动健康打卡第一版

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# 安装使用
+## win10系统
+1. 下载安装(Node)[https://nodejs.org/en/]，选择LTS版
+2. 安装cnpm，并设置为默认，终端输入
+```
+npm install -g cnpm --registry=https://registry.npm.taobao.org
+cnpm install
+```
+3. 右击此电脑 -> 管理 -> 任务计划程序 -> 操作 -> 创建基本任务 -> 名称和描述自己随便写 -> 下一步 -> 每天 -> 下一步 -> 设置执行脚本时间 -> 下一步 -> 启动程序 -> 下一步 -> 添加AutoClock.bat文件 -> 下一步 -> 完成
+## Ubuntu系统
---
+++ main.js
@@ -0,0 +1,95 @@
+const puppeteer = require("puppeteer"); // 模拟操作
+const YAML = require("yamljs"); // 解析YAML文件
+const fs = require("fs"); // 解析文件
+const path = require("path"); // 路径相关
+// 读取配置文件
+const yaml_file = path.join(__dirname, "./config.yaml");
+const cfg = YAML.parse(fs.readFileSync(yaml_file).toString());
+const delayTime = cfg.setting.delayTime;
+// 打开浏览器
+async function openBrowser() {
+  const browser = await puppeteer.launch({
+    slowMo: 20, // 操作减慢
+    headless: false, // 是否隐藏浏览器
+    defaultViewport: { width: 960, height: 540 }, // 浏览器窗口大小
+    ignoreHTTPSErrors: false, // 忽略 https 报错
+    args: [`--window-size=${960},${540}`, "--no-sandbox"],
+  });
+  return browser;
+}
+// 延迟一定时间
+async function delay(time) {
+  return new Promise((r) => setTimeout(r, time));
+}
+// 开始打卡
+async function clock(userInfo) {
+  let browser;
+  try {
+    // 打开页面
+    browser = await openBrowser();
+    const page = await browser.newPage();
+    await page.goto("https://seicwxbz.seu.edu.cn/self-service/");
+    await delay(delayTime);
+    // 账号
+    const username = await page.waitForSelector("#username");
+    await username.type(String(userInfo.username), { delay: 1 });
+    // 密码
+    const password = await page.waitForSelector("#password");
+    await password.type(String(userInfo.password), { delay: 1 });
+    // 点击登录
+    const login = await page.waitForSelector("button[class*=login]");
+    await login.click();
+    await page.waitForNavigation();
+    await delay(delayTime);
+    // 点击健康申报
+    const report = await page.waitForSelector("#right-inner > div:first-child");
+    await report.click();
+    await page.waitForNavigation();
+    await delay(delayTime);
+    // 等待列表加载后
+    await page.waitForSelector(".mint-loadmore > .mint-loadmore-content");
+    // 点击新增
+    const add = await page
+      .waitForSelector(".pjcse52gj > button[class*=bottom155]", {
+        timeout: 5000,
+      })
+      .catch(() => {
+        throw new Error(`---${userInfo.username} 已经打卡---`);
+      });
+    await add.click();
+    await page.waitForNavigation();
+    await delay(delayTime);
+    // 填入体温
+    const temperature = await page.waitForSelector(
+      "input[placeholder*=请输入当天晨检体温]"
+    );
+    temperature.type(String(36 + Number(Math.random().toFixed(1))));
+    // 点击提交
+    const submit = await page.waitForSelector(
+      ".OPjctwlgzsl > button[class*=mint-button--large]"
+    );
+    submit.click();
+    // 点击确认
+    const sure = await page.waitForSelector(
+      ".mint-msgbox > .mint-msgbox-btns > .mint-msgbox-confirm",
+      { visible: true }
+    );
+    sure.click();
+    // 流程结束
+    await page.waitForNavigation();
+    await delay(delayTime);
+    console.log(`---${userInfo.username} 打卡成功---`);
+  } catch (e) {
+    console.log(e.message);
+    console.log(`---${userInfo.username} 打卡失败---`);
+  } finally {
+    browser && await browser.close();
+  }
+}
+
+const main = async () => {
+  for (const user of cfg.users) {
+    await clock(user);
+  }
+};
+main();
